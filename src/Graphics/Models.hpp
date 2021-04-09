d245bd6f8d163b514edc61d44402e2f250d49323
Tmforshaw <tmforshaw@outlook.com>
2021-04-09 14:38:26 +0100

    Move Cube Vertices Comment

--- src/Graphics/Models.hpp
+++ src/Graphics/Models.hpp
@@ -80,3 +80,33 @@ public:
 	inline const std::vector<Vertex>&		   GetVertices() const { return m_vertices; }
 	inline const std::vector<IndexBufferType>& GetIndices() const { return m_indices; }
 };
+
+// // clang-format off
+// const std::vector<Vertex> cubeVertices = {
+// 	{ { -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f } },
+// 	{ {  0.5f, -0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f } },
+// 	{ {  0.5f,  0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f } },
+// 	{ { -0.5f,  0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f } },
+// 	{ { -0.5f, -0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f } },
+// 	{ {  0.5f, -0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 0.0f } },
+// 	{ {  0.5f,  0.5f,  0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f } },
+// 	{ { -0.5f,  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 1.0f } },
+// 	{ {  0.5f, -0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f } },
+// 	{ { -0.5f,  0.5f,  0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f } },
+// 	{ { -0.5f,  0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 1.0f } },
+// 	{ { -0.5f, -0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } },
+// 	{ { -0.5f,  0.5f,  0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f } },
+// 	{ {  0.5f,  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f } },
+// 	{ {  0.5f, -0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f } },
+// 	{ {  0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f } }
+// };
+// // clang-format on
+
+// const std::vector<uint16_t> cubeIndices = {
+// 	0, 2, 1, 2, 0, 3,	  // Front Face
+// 	4, 5, 6, 6, 7, 4,	  // Back Face
+// 	9, 10, 11, 11, 4, 9,  // Right Face
+// 	13, 15, 2, 15, 13, 8, // Left Face
+// 	11, 14, 5, 5, 4, 11,  // Bottom Face
+// 	3, 13, 2, 13, 3, 12	  // Top Face
+// };
